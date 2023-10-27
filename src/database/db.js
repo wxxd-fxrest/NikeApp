778afe9e1f922827f55d5580f5d0762ded106909
fxrest704 <fxrest704@gmail.com>
2023-10-27 19:28:38 +0900

    complete

--- src/database/db.js
+++ src/database/db.js
@@ -3,13 +3,21 @@ require("dotenv").config();
 
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PW}@cluster0.8qybqfs.mongodb.net/?retryWrites=true&w=majority`
 
-const client = new MongoClient(uri); 
+let client;
 
-const database = client.db('test');
-const products = database.collection('products');
-const orders = database.collection('orders');
+const getDB = () => {
+    if(!client) {
+        client = new MongoClient(uri); 
+    }
 
-module.exports = {
-    products,
-    orders,
+    const database = client.db('test');
+    const products = database.collection('products');
+    const orders = database.collection('orders');
+
+    return {
+        products, 
+        orders,
+    }
 };
+
+module.exports = getDB;
